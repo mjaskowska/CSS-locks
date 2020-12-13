3227947104fac2697a9a057101c3cf139adcbceb
Magda <jaskowska.magda@gmail.com>
2020-12-13 16:00:46 +0000

    fix font size function

--- font-size.js
+++ font-size.js
@@ -1,79 +1,66 @@
-
-const minWidthInput = document.querySelector('.min-width');
-const maxWidthInput = document.querySelector('.max-width');
-
-const fontMinPxInput = document.querySelector('.min-font');
-const fontMaxPxInput = document.querySelector('.max-font');
-
-
-// font-size calc
-
 function countFontSize() {
+    const cacheDOM = {
+        minWidth: document.querySelector('.min-width'),
+        maxWidth: document.querySelector('.max-width'),
+        fontMinPx: document.querySelector('.min-font'),
+        fontMaxPx: document.querySelector('.max-font'),
+        errorMsg: document.querySelector('.errorMsg'),
+        codeContainer: document.querySelector('.result-bkg'),
+        resetBtn: document.querySelector('.reset')
+    }
 
-    const minWidth = parseFloat(minWidthInput.value);
-    const maxWidth = parseFloat(maxWidthInput.value);
-    const fontMinPx = parseFloat(fontMinPxInput.value);
-    const fontMaxPx = parseFloat(fontMaxPxInput.value);
-    
+    const minWidth = parseFloat(cacheDOM.minWidth.value);
+    const maxWidth = parseFloat(cacheDOM.maxWidth.value);
+    const fontMinPx = parseFloat(cacheDOM.fontMinPx.value);
+    const fontMaxPx = parseFloat(cacheDOM.fontMaxPx.value);
+
     const fontMinRem = fontMinPx / 16;
 
-
     const mFont = (fontMaxPx - fontMinPx) / (maxWidth - minWidth);
-    const bFont = 0 - (mFont * minWidth);
-    const mFontvw = mFont * 100;
     const fontDiffPx = fontMaxPx - fontMinPx;
 
-    const bFont1 = Math.round((bFont + Number.EPSILON) * 1000) / 1000;
-    const mFontvw1 = Math.round((mFontvw + Number.EPSILON) * 1000) / 1000;
+    const bFont = Math.round(((0 - (mFont * minWidth)) + Number.EPSILON) * 1000) / 1000;
+    const mFontvw = Math.round(((mFont * 100) + Number.EPSILON) * 1000) / 1000;
 
-    if (minWidth === '' || maxWidth === '' || fontMinPx === '' || fontMaxPx === '') {
-
-        const errorMsg = document.querySelector('.errorMsg');
-        errorMsg.innerHTML = "Fill all the fields with valid numbers.";
-        errorMsg.style.display = "block";
-    }
-    else {
-        const errorMsg = document.querySelector('.errorMsg');
-        errorMsg.style.display = "none";
-
-
+   
+   
+    if (cacheDOM.minWidth.value >0 && cacheDOM.maxWidth.value >0 && cacheDOM.fontMinPx.value >0 && cacheDOM.fontMaxPx.value >0)
+    {
+        cacheDOM.errorMsg.style.display = "none";
         const code1 = document.createElement('div');
         const code2 = document.createElement('div');
         const code3 = document.createElement('div');
+
         code1.setAttribute('class', 'result-code');
         code2.setAttribute('class', 'result-code');
         code3.setAttribute('class', 'result-code');
-        const codeContainer = document.querySelector('.result-bkg')
-        codeContainer.appendChild(code1);
-        codeContainer.appendChild(code2);
-        codeContainer.appendChild(code3);
+        
+        cacheDOM.codeContainer.appendChild(code1);
+        cacheDOM.codeContainer.appendChild(code2);
+        cacheDOM.codeContainer.appendChild(code3);
 
         code1.innerHTML = `font-size: ${fontMinRem}rem;`
-        code2.innerHTML = `@media (min-width: ${minWidth}px) { font-size: calc(${fontMinRem}rem + ${mFontvw1}vw + ${bFont1}px); }`
+        code2.innerHTML = `@media (min-width: ${minWidth}px) { font-size: calc(${fontMinRem}rem + ${mFontvw}vw + ${bFont}px); }`
         code3.innerHTML = `@media (min-width: ${maxWidth}px) { font-size: calc(${fontMinRem}rem + ${fontDiffPx}px); }`
     }
+    else {
+        cacheDOM.errorMsg.innerHTML = "Fill all the fields with valid numbers.";
+        cacheDOM.errorMsg.style.display = "block";
+    }
 
-    // result testing
-    //     console.log(`font-size: ${fontMinRem}rem;`)
-    //     console.log(`@media (min-width: ${minWidth}px) { font-size: calc(${fontMinRem}rem + ${mFontvw1}vw + ${bFont1}px); }`)
-    //     console.log(`@media (min-width: ${maxWidth}px) { font-size: calc(${fontMinRem}rem + ${fontDiffPx}px); }`)
+    const reset = () => {
+
+        cacheDOM.minWidth.value = '';
+        cacheDOM.maxWidth.value = '';
+        cacheDOM.fontMinPx.value = '';
+        cacheDOM.fontMaxPx.value = '';
+
+        cacheDOM.errorMsg.style.display = "none";
+        cacheDOM.codeContainer.innerHTML = '';
+    
+    }
+    
+    cacheDOM.resetBtn.addEventListener('click', reset);
 }
-
-const calculateBtn1 = document.querySelector('.calc1');
+const calculateBtn1 = document.querySelector('.calc1')
 calculateBtn1.addEventListener('click', countFontSize);
-
-
-const reset = () => {
-
-    minWidthInput.value = '';
-    maxWidthInput.value = '';
-    fontMinPxInput.value = '';
-    fontMaxPxInput.value = '';
-
-    const codeContainer = document.querySelector('.result-bkg')
-    codeContainer.innerHTML = '';
-
-}
-const resetBtn = document.querySelector('.reset');
-resetBtn.addEventListener('click', reset);
-
